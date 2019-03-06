3f45970d9667e2f2b57e8d1282067a412c4a5b35
Hellsmasher24 <deep.kamani24@gmail.com>
2019-03-06 10:33:30 -0500

    Update login.php

--- login.php
+++ login.php
@@ -39,7 +39,7 @@
             </div>
       </nav>
       <main>
-            <section class="col-md-6 col-lg-6 container" id="app">
+            <section class="col-md-6 col-lg-6 col-xl-6 container" id="app">
                   <header>
                         <h1 class="text-center">Log In</h1>
                   </header>
